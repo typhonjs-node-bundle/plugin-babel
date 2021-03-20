e55a4c6b25ce545113a49d094a3ec658f52d429d
typhonrt <support@typhonrt.org>
2021-03-20 03:53:26 -0700

    added error parser filter for Babel

--- src/hooks/bundle-load-plugins.js
+++ src/hooks/bundle-load-plugins.js
@@ -11,10 +11,17 @@ export default async function(options)
 {
    try
    {
+      globalThis.$$eventbus.trigger('log:debug', `plugin-babel init hook running '${options.id}'.`);
+
       await globalThis.$$pluginManager.addAsync({ name: PluginLoader.packageName, instance: PluginLoader,
        options: { id: options.id, flagsModule: options.flagsModule } });
 
-      globalThis.$$eventbus.trigger('log:debug', `plugin-babel init hook running '${options.id}'.`);
+      // Add a filter to exclude any errors generating from Babel so the calling code is highlighted.
+      globalThis.$$eventbus.trigger('typhonjs:util:error:parser:filter:add', {
+         type: 'exclusive',
+         name: '@babel',
+         filterString: '@babel'
+      });
    }
    catch (error)
    {
